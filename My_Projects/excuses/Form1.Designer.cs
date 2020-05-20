12a7d5f256842f95ec0cc01ca70559e9e577cbb7
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-05-20 21:30:20 +0200

    Start 2020-05-20 12:00   Koniec 2020-05-20 20:00

---
+++ My_Projects/excuses/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace excuses
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folder = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.randomExcuse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.description = new System.Windows.Forms.TextBox();
+            this.results = new System.Windows.Forms.TextBox();
+            this.lastUsed = new System.Windows.Forms.DateTimePicker();
+            this.fileDate = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // folder
+            // 
+            this.folder.Location = new System.Drawing.Point(16, 257);
+            this.folder.Name = "folder";
+            this.folder.Size = new System.Drawing.Size(75, 23);
+            this.folder.TabIndex = 0;
+            this.folder.Text = "Folder";
+            this.folder.UseVisualStyleBackColor = true;
+            this.folder.Click += new System.EventHandler(this.folder_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(119, 257);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 1;
+            this.save.Text = "Zapisz";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(214, 257);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 2;
+            this.open.Text = "Otworz";
+            this.open.UseVisualStyleBackColor = true;
+            // 
+            // randomExcuse
+            // 
+            this.randomExcuse.Location = new System.Drawing.Point(296, 257);
+            this.randomExcuse.Name = "randomExcuse";
+            this.randomExcuse.Size = new System.Drawing.Size(112, 23);
+            this.randomExcuse.TabIndex = 3;
+            this.randomExcuse.Text = "Losowa wymowka";
+            this.randomExcuse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Wymowka";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ostatnio uzyte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Wyniki";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data pliku";
+            // 
+            // description
+            // 
+            this.description.Location = new System.Drawing.Point(165, 41);
+            this.description.Name = "description";
+            this.description.Size = new System.Drawing.Size(155, 20);
+            this.description.TabIndex = 7;
+            // 
+            // results
+            // 
+            this.results.Location = new System.Drawing.Point(165, 86);
+            this.results.Name = "results";
+            this.results.Size = new System.Drawing.Size(155, 20);
+            this.results.TabIndex = 8;
+            // 
+            // lastUsed
+            // 
+            this.lastUsed.Location = new System.Drawing.Point(165, 141);
+            this.lastUsed.Name = "lastUsed";
+            this.lastUsed.Size = new System.Drawing.Size(200, 20);
+            this.lastUsed.TabIndex = 9;
+            // 
+            // fileDate
+            // 
+            this.fileDate.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fileDate.Location = new System.Drawing.Point(165, 201);
+            this.fileDate.Name = "fileDate";
+            this.fileDate.Size = new System.Drawing.Size(191, 23);
+            this.fileDate.TabIndex = 10;
+            this.fileDate.Text = "label5";
+            this.fileDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 310);
+            this.Controls.Add(this.fileDate);
+            this.Controls.Add(this.lastUsed);
+            this.Controls.Add(this.results);
+            this.Controls.Add(this.description);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.randomExcuse);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.folder);
+            this.Name = "Form1";
+            this.Text = "Program do zarzadzania wymowkami";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button folder;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button randomExcuse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox description;
+        private System.Windows.Forms.TextBox results;
+        private System.Windows.Forms.DateTimePicker lastUsed;
+        private System.Windows.Forms.Label fileDate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+

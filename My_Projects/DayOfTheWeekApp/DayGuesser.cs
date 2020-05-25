d410b8f502dd829b0d2abc6b6f1e30aacc1c7c97
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-05-25 23:32:23 +0200

    Start 2020-05-25 14:00  Koniec 2020-05-25 22:00

---
+++ My_Projects/DayOfTheWeekApp/DayGuesser.cs
@@ -0,0 +1,51 @@
+﻿using DayOfTheWeekApp.Core;
+using DayOfTheWeekApp.Core.Enums;
+using System;
+
+namespace DayOfTheWeekApp
+{
+     public class DayGuesser
+    {
+        public DayCalculator Calculator { get; set; }
+        public DayOfTheWeek UserDayOfTheWeek { get; set; }
+        public DateTimeOffset UserDateOfBrith { get; set; }
+
+        public void IntroducesTheApplication()
+        {
+            Console.WriteLine("WELCOME IN APP");
+
+            Calculator = new DayCalculator();
+        }
+
+        public void AskUserforTHeirDateOfBrith()
+        {
+            Console.WriteLine("Podaj mi prosze swoja date urodzina");
+           var userDate = Console.ReadLine();
+           var succeded = DateTimeOffset.TryParse( userDate, out var date);
+            if (succeded)
+            {
+                UserDateOfBrith = date;
+                return;
+            }
+            Console.WriteLine("Format daty byl zly.Prosze podac go w mm/dd/yyyy");
+            AskUserforTHeirDateOfBrith();
+
+
+
+        }
+
+        public void CalculateTheDateOfWeek()
+        {
+            if(UserDateOfBrith == null)
+            {
+                Console.WriteLine("Probowano obliczyc dzien tygodnia bez podania daty urodenia");
+            }
+           UserDayOfTheWeek = Calculator.CalculateDayOfTheWeek(UserDateOfBrith);
+        }
+
+        public void PrintDayOfTheWeek()
+        {
+            Console.WriteLine("Obliczony dzien tygodnia to " + UserDayOfTheWeek);
+        }
+    }
+}

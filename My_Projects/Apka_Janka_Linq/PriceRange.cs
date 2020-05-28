7934bc9fb78576ebae0e46ccc4754d9f1d260646
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-05-28 22:22:10 +0200

    Start 2020-05-28 16:00 Koniec 2020-05-28 22:00

---
+++ My_Projects/Apka_Janka_Linq/PriceRange.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Apka_Janka_Linq
+{
+    enum PriceRange { Cheap, Midrange, Expensive }
+}
---
+++ My_Projects/LINQ(exercisev2)/Comic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQ_exercisev2_
+{
+    public class Comic
+    {
+
+        public string Name { get; set; }
+        public  int Issue { get; set; }
+
+
+    }
+}

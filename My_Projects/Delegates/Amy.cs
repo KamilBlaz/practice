9c5762876457e8ce6c32eca5f7f9c68324df8a9d
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-05-29 22:23:31 +0200

    Start 2020-05-29 13:30 Koniec 2020-05-29 21:30

---
+++ My_Projects/Delegates/Amy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Amy
+    {
+
+        public GetSecretIngredient MySecretIngredientMethod
+        {
+            get{
+                return new GetSecretIngredient(AmysSecretIngredient);
+
+            }
+        }
+
+        private string AmysSecretIngredient(int amount)
+        {
+            if (amount < 10)
+                return amount.ToString() + " puszek sardynek -- potrzebujesz wiecej!";
+            else
+                return amount.ToString() + " puszek sarydnek";
+        }
+    }
+}

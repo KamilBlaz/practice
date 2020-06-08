122e6e235b00d66e0a7012dfab0fa7dd81946921
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-06-08 22:25:46 +0200

    Start 2020-06-08 14:00  Koniec 2020-06-08 22:00

---
+++ My_Projects/Invaders/Model/ShotMovedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace Invaders.Model
+{
+    class ShotMovedEventArgs:EventArgs
+    {
+        public Shot Shot { get; private set; }
+        public bool Disappeared { get; private set; }
+
+        public ShotMovedEventArgs(Shot shot, bool disappeared)
+        {
+            Shot = shot;
+            Disappeared = disappeared;
+        }
+
+
+    }
+}

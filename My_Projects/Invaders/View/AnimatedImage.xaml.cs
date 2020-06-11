1878873f2f68b62e3c511b9fb80c0f008b3844ec
Kamil Błaż <48087257+blusik99@users.noreply.github.com>
2020-06-11 22:20:19 +0200

    Start 2020-05-25 14:00  Koniec 2020-05-25 22:00

---
+++ My_Projects/Invaders/View/AnimatedImage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Invaders.View
+{
+    using Windows.UI.Xaml.Media.Animation;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    public sealed partial class AnimatedImage : UserControl
+    {
+        public AnimatedImage()
+        {
+            this.InitializeComponent();
+        }
+
+        public AnimatedImage(IEnumerable<string> imageNames, TimeSpan interval)
+            : this()
+        {
+            StartAnimation(imageNames, interval);
+        }
+
+        public void StartAnimation(IEnumerable<string> imageNames, TimeSpan interval)
+        {
+            Storyboard storyboard = new Storyboard();
+            ObjectAnimationUsingKeyFrames animation = new ObjectAnimationUsingKeyFrames();
+            Storyboard.SetTarget(animation, image);
+            Storyboard.SetTargetProperty(animation, "Source");
+
+            TimeSpan currentInteval = TimeSpan.FromMilliseconds(0);
+            foreach (string imageName in imageNames)
+            {
+                ObjectKeyFrame keyFrame = new DiscreteObjectKeyFrame();
+                keyFrame.Value = InvadersHelper.CreateImageFromAssets(imageName);
+                keyFrame.KeyTime = currentInteval;
+                animation.KeyFrames.Add(keyFrame);
+                currentInteval = currentInteval.Add(interval);
+            }
+
+            storyboard.RepeatBehavior = RepeatBehavior.Forever;
+            storyboard.AutoReverse = true;
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        public void InvaderShot()
+        {
+            invaderShotStoryboard.Begin();
+        }
+
+        public void StartFlashing()
+        {
+            flashStoryboard.Begin();
+        }
+
+        public void StopFlashing()
+        {
+            flashStoryboard.Stop();
+        }
+    }
+}
